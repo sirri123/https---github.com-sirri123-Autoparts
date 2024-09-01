892af5d9f580b8361fd334eeb007cf0f2c0f154d
Soumya <racrac786po@gmail.com>
2024-09-01 08:59:49 +0530

    commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Hamburger Menu Toggle
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
